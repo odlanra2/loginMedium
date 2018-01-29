e1dff62c4644dbef51dc62a1972cb16aa3137f3e
odlara2 <arnaldolameda@gmail.com>
2018-01-29 15:30:00 -0400

    loginMedium

--- application/config/database.php
+++ application/config/database.php
@@ -60,7 +60,7 @@ $active_record = TRUE;
 */
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'adminadmin';
+$db['default']['password'] = '';
 $db['default']['database'] = 'simplelogin';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
